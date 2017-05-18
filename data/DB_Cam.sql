4736f6d9508915e3478062472d8e17df3005cc8b
Dennis <fortuna.edingen.1910@gmail.com>
2017-05-18 14:14:28 +0200

    SQL zum einfügen

---
+++ data/DB_Cam.sql
@@ -0,0 +1,24 @@
+﻿--Erstellen einer Automtischen id für User
+CREATE SEQUENCE wai_cam_id_seq START WITH 1 INCREMENT BY 1;
+
+--Erstellen einer Tabelle
+CREATE TABLE public.wai_cam
+(
+  id integer NOT NULL DEFAULT nextval('wai_cam_id_seq'::regclass),
+  name character varying(1024),
+  uri character varying(1024),
+  interval character varying(1024),
+  CONSTRAINT wai_cam_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+--Einfügen von Inhalt der Tabelle
+INSERT INTO public.wai_cam (name, uri, interval) VALUES ('WER', 'WAS', 'WO');
+INSERT INTO public.wai_cam (name, uri, interval) VALUES ('warum', 'wieso', 'weshalb');
+
+
+--Anzeigen der Tabelle
+SELECT * FROM public.wai_cam
+
---
+++ data/DB_User.sql
@@ -0,0 +1,31 @@
+﻿--Erstellen einer Automtischen id für User
+CREATE SEQUENCE wai_user_id_seq START WITH 1 INCREMENT BY 1;
+
+--Erstellen einer Tabelle
+CREATE TABLE public.wai_user
+(
+  id integer NOT NULL DEFAULT nextval('wai_user_id_seq'::regclass),
+  vorname character varying,
+  nachname character varying,
+  can_mod_cam boolean NOT NULL DEFAULT false,
+  can_mod_user boolean NOT NULL DEFAULT false,
+  can_delegate_cam boolean NOT NULL DEFAULT false,
+  can_see_all boolean NOT NULL DEFAULT false,
+  salt character varying(120),
+  username bit varying(1024),
+  CONSTRAINT wai_user_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+--Einfügen von Inhalt der Tabelle
+INSERT INTO public.wai_user (vorname, nachname, can_mod_cam, can_mod_user, can_delegate_cam, can_see_all, salt, username)
+VALUES ('Dennis', 'Schmidt', 'false', 'true', 'true' ,'true', 'Test', '10');
+INSERT INTO public.wai_user (vorname, nachname, can_mod_cam, can_mod_user, can_delegate_cam, can_see_all, salt, username)
+VALUES ('Aron', 'Unbekannt', 'true', 'false', 'true' ,'false', 'Test2', '10');
+
+
+--Anzeigen der Tabelle
+SELECT * FROM public.wai_user
+
