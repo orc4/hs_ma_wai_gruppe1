0df66f98995c784c795e64008c94ec9a94145a5e
aaron <Aaron2222@gmx.de>
2017-05-23 00:15:34 +0200

    exceptions rausgeschmissen

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,40 +1,52 @@
 package storage;
 
 import java.sql.Date;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.naming.NamingException;
 
 import data_model.Cam;
 import data_model.Picture;
 import data_model.User;
 
 public interface Storage {
-	// User
-	public List<User> getListUser();													// Erledigt
-	public void addUser(User user);														// Erledigt
-	public void delUser(long id);														// Erledigt
-	public void editUser(long id, User user);											// Erledigt
-	public User getUserByName(String username);											// 
-	public User getUserById(Long id);													// 
-	public void setUserCamAllow(long userId, long camId);								// 
-	public void unsetUserCamAllow(long userId, long camId);								// 
+	public void addCam(Cam cam); //
+
+	public void addPic(Picture pic); //
+
+	public void addUser(User user); // Erledigt
+
+	public void delCam(long id); // Erledigt
+
+	public void delUser(long id); // Erledigt
+
+	public void editCam(long id, Cam newCam); //
+
+	public void editUser(long id, User user); // Erledigt
+
+	public List<Cam> getCamForUser(long User); //
 
 	// Cameras
-	public List<Cam> getCamList();														// Erledigt
-	public List<Cam> getCamForUser(long User);											// 
-	public Object addCam(Cam cam) throws SQLException, NamingException;					// 
-	public void delCam(long id);														// Erledigt 
-	public void editCam(long id, Cam newCam);											// 
-	
-	// Picture
-	public List<Picture> getPictureBetween(long camId, Date from, Date to, long limit);	// 
-	public void addPic(Picture pic);													// 
-	public Picture getPicture(long id);													// 
-	
+	public List<Cam> getCamList(); // Erledigt
+
+	public List<Date> getDaysWithPictures(long camId, Date from, Date to); //
+
+	public List<Date> getHoursWithPictures(long camId, Date from, Date to); //
+	// User
+
+	public List<User> getListUser(); // Erledigt
 	// Sonstiges
-	public List<Date> getMonthsWithPictures(long camId, Date from, Date to);			// 
-	public List<Date> getDaysWithPictures(long camId, Date from, Date to);				// 
-	public List<Date> getHoursWithPictures(long camId, Date from, Date to);				// 
+
+	public List<Date> getMonthsWithPictures(long camId, Date from, Date to); //
+
+	public Picture getPicture(long id); //
+	// Picture
+
+	public List<Picture> getPictureBetween(long camId, Date from, Date to, long limit); //
+
+	public User getUserById(Long id); //
+
+	public User getUserByName(String username); //
+
+	public void setUserCamAllow(long userId, long camId); //
+
+	public void unsetUserCamAllow(long userId, long camId); //
 }
