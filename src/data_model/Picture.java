abdee642acc1153244a4c38c44907ad52acc297f
aaron <Aaron2222@gmx.de>
2017-05-23 13:46:14 +0200

    camid reingelegt

--- src/data_model/Picture.java
+++ src/data_model/Picture.java
@@ -5,35 +5,36 @@ import java.sql.Date;
 public class Picture {
 	// Private Variablen
 	private long id;
-	private Cam cam;
+	private long camId;
 	private Date date;
 	private String path;
 
 	// Konstruktoren
-	public Picture(Date date, String path) {
+	public Picture(Date date, String path, long camId) {
 		this.date = date;
 		this.path = path;
 	}
-	
-	public Picture(long id, Date date, String path) {
+
+	public Picture(long id, Date date, String path, long camId) {
 		this.id = id;
 		this.date = date;
 		this.path = path;
+		this.camId = camId;
 	}
-	
+
+	public long getCam() {
+		return (camId);
+	}
+
+	public Date getDate() {
+		return (date);
+	}
+
 	// Gettermethoden
 	public long getId() {
 		return (id);
 	}
-	
-	public Cam getCam() {
-		return (cam);
-	}
-	
-	public Date getDate() {
-		return (date);
-	}
-	
+
 	public String getPath() {
 		return (path);
 	}
